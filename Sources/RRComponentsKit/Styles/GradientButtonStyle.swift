2fcbc9c4fc4f9264b287d2227ef5975eb7e3c2d8
Rudrank Riyam <rudrankriyam@gmail.com>
2021-03-23 16:51:13 +0530

    Update GradientButtonStyle

--- Sources/RRComponentsKit/Styles/GradientButtonStyle.swift
+++ Sources/RRComponentsKit/Styles/GradientButtonStyle.swift
@@ -14,12 +14,11 @@ public struct GradientButtonStyle: ButtonStyle {
         configuration.label
             .foregroundColor(.white)
             .font(weight: .semiBold, style: .title3)
-            .padding(.vertical, 12)
-            .frame(minWidth: 0, maxWidth: .infinity, alignment: .center)
+            .padding(.vertical)
+            .frame(maxWidth: .infinity, alignment: .center)
             .background(AnimatedGradientView().cornerRadius(12))
-            .padding(.bottom, 8)
             .scaleEffect(configuration.isPressed ? 0.98 : 1.0)
             .opacity(configuration.isPressed ? 0.9 : 1.0)
-            .padding()
+            .padding([.horizontal, .top])
     }
 }
