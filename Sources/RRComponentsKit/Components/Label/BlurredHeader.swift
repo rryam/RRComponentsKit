714517ebe2edcb993b377f8f32e58145248461dc
Rudrank Riyam <rudrankriyam@gmail.com>
2021-03-29 16:44:41 +0530

    Customise BlurredHeader with style

--- Sources/RRComponentsKit/Components/Label/BlurredHeader.swift
+++ Sources/RRComponentsKit/Components/Label/BlurredHeader.swift
@@ -10,17 +10,19 @@ import SwiftUI
 public struct BlurredHeader: View {
     private var title: String
     private var weight: FontWeight
+    private var style: UIFont.TextStyle
 
-    public init(_ title: String, weight: FontWeight) {
+    public init(_ title: String, weight: FontWeight = .bold, style: UIFont.TextStyle = .footnote) {
         self.title = title
         self.weight = weight
+        self.style = style
     }
 
     public var body: some View {
         Text(title)
             .tracking(1.0)
             .multilineTextAlignment(.center)
-            .font(weight: weight, style: .footnote)
+            .font(weight: weight, style: style)
             .foregroundColor(.white)
             .padding(8)
             .background(RoundedRectangle(cornerRadius: 6, style: .continuous)
