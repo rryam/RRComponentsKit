1a1594238544d1619e3d0d61aa35965e2a291cda
Rudrank Riyam <rudrankriyam@gmail.com>
2021-07-12 01:20:07 +0530

    MovingNumbersView dependency

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "RRComponentsKit",
-            dependencies: []),
+            dependencies: ["MovingNumbersView"]),
         .testTarget(
             name: "RRComponentsKitTests",
             dependencies: ["RRComponentsKit"]),
