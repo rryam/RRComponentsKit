707273da6a77b3a9428d4b2652db3e210e5e6449
Rudrank Riyam <rudrankriyam@gmail.com>
2021-07-12 01:16:50 +0530

    add dependency

--- Package.swift
+++ Package.swift
@@ -14,6 +14,7 @@ let package = Package(
             targets: ["RRComponentsKit"]),
     ],
     dependencies: [
+        .package(url: "https://github.com/aunnnn/MovingNumbersView.git", from: "0.1.0"),
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
     ],
