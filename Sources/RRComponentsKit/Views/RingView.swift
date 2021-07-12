ebfe1c3f37c272f5f997cef8e645275bf228ec6a
Rudrank Riyam <rudrankriyam@gmail.com>
2021-07-13 03:36:28 +0530

    Add RingView

--- Sources/RRComponentsKit/Views/RingView.swift
+++ Sources/RRComponentsKit/Views/RingView.swift
@@ -7,55 +7,135 @@
 
 import SwiftUI
 
-public struct RingView: View {
-    var currentScore: Double
-    var individualPoints: String
-    var leftColor: Color
-    var rightColor: Color
+public struct ScoreView: View {
+    var seconds: Double
+    var score: Double
+    var highScore: Double
     
-    private let lineWidth: CGFloat = 50
-    private let dash: [CGFloat] = [4, 10]
+    public init(_ score: Double, _ highScore: Double, seconds: Double = 15.0) {
+        self.seconds = seconds
+        self.highScore = highScore
+        self.score = score
+    }
     
     public var body: some View {
         VStack {
-            Text("OVERALL SCORE")
-                .font(weight: .bold, style: .title3)
-            ZStack {
-                
-                CustomCircle(leftColor: .ringBackground, rightColor: Color.ringBackground.opacity(0.7), currentScore: 1)
-                CustomCircle(leftColor: leftColor, rightColor: rightColor, currentScore: currentScore)
-                
-                VStack {
-                    Text(individualPoints)
-                        .font(weight: .bold, style: .title1)
-                        .accessibility(label: Text("\(individualPoints) points"))
-                    
-                    Text("OUT OF 100")
-                        .font(style: .callout)
-                        .foregroundColor(.secondary)
-                }
-                .accessibilityElement(children: .combine)
+            AnalyticsScoreView(highScore: highScore, secondsTaken: seconds) {
+                UserScoreView(score)
             }
-            .offset(y: 40)
-            .padding(.bottom, -40)
+        }
+        .padding(.top)
+    }
+}
+
+struct AnalyticsScoreView<Content: View>: View {
+    var highScore: Double
+    var secondsTaken: Double
+    var scoreRing: () -> Content
+    
+    var body: some View {
+        HStack {
+            Spacer()
+            
+            scoreRing()
+            
+            Spacer()
+            
+            Divider()
+                .layoutPriority(-1)
+            
+            Spacer()
+
+            VStack {
+                AnalyticsInfoView(title: "HIGH \nSCORE", value: String(format: "%.1f", highScore))
+                
+                Spacer()
+                AnalyticsInfoView(title: "SECONDS \nTAKEN", value: String(describing: secondsTaken))
+            }
+            .layoutPriority(-1)
+            
+            Spacer()
         }
     }
 }
 
-struct CustomCircle: View {
-    var leftColor: Color
-    var rightColor: Color
-    var currentScore: Double
+struct AnalyticsInfoView: View {
+    var title: String
+    var value: String
     
-    var gradient: Gradient {
-        Gradient(colors: [rightColor, leftColor])
+    var body: some View {
+        VStack {
+            Text(title)
+                .kerning(1)
+                .font(weight: .light, style: .caption2)
+            
+            Text(value)
+                .kerning(1)
+                .font(weight: .bold, style: .title2)
+                .foregroundColor(.accentColor)
+        }
+        .multilineTextAlignment(.center)
+    }
+}
+
+struct UserScoreView: View {
+    var score: Double
+    
+    public init(_ score: Double) {
+        self.score = score
     }
     
     var body: some View {
-        Circle()
-            .trim(from: 0, to: CGFloat(currentScore) / 2)
-            .stroke(LinearGradient(gradient: gradient, startPoint: .leading, endPoint: .trailing),
-                    style: StrokeStyle(lineWidth: 80, lineCap: .butt, lineJoin: .bevel, dash: [10, 11]))
-            .rotationEffect(.degrees(-180))
+        ZStack {
+            ProgressRingView(progress: score / 100)
+            
+            VStack(spacing: 0) {
+            Text("SCORE")
+                .kerning(1)
+                .font(weight: .light, style: .caption1)
+            
+                Text(String(format: "%.1f", score))
+                    .kerning(1)
+                    .font(weight: .black, style: .largeTitle)
+                    .foregroundColor(.accentColor)
+                    .accessibility(label: Text("\(String(format: "%.1f", score)) points"))
+            }
+            .accessibilityElement(children: .combine)
+        }
+    }
+}
+
+struct ScoreView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScoreView(85.0, 100.0).accentColor(Color.red)
+    }
+}
+
+struct ProgressRingView: View {
+    var progress: CGFloat
+    var lineWidth: CGFloat = 20
+    var size: CGFloat = 150
+    var gradient = [.accentColor, Color.accentColor.opacity(0.5)]
+    
+    var body: some View {
+        ZStack {
+            Circle()
+                .stroke(Color(.systemGray6), style:
+                            StrokeStyle(lineWidth: lineWidth))
+                .frame(width: size, height: size)
+            
+            Circle()
+                .trim(from: 1 - progress, to: 1)
+                .stroke(
+                    LinearGradient(
+                        gradient: Gradient(colors: gradient),
+                        startPoint: .leading, endPoint: .trailing),
+                    style: StrokeStyle(lineWidth: lineWidth, lineCap: .round)
+                )
+                .rotationEffect(Angle(degrees: 90))
+                .rotation3DEffect(Angle(degrees: 180),
+                                  axis: (x: 1, y: 0, z: 0))
+                .frame(width: size, height: size)
+        }
     }
 }
